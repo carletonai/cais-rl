5b05798fda6300f9d541fbcc7f059eaed4cb0b83
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-06-02 22:13:28 -0400

    added feature request template

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'MuZero'
+copyright = '2024, Your Name'
+author = 'Your Name'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'torch': ('https://pytorch.org/docs/stable/', None),
+} 
